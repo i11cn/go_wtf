0a0854d425c3e3a3d79cde84df4cf83c5f066304
snower <snower@snower.org>
2016-01-21 22:39:05 +0800

    辅助类的方法转变一下

--- misc.go
+++ misc.go
@@ -8,48 +8,48 @@ type (
 	Convert string
 )
 
-func (s *Convert) ToInt() (int, error) {
-	ret, err := strconv.ParseInt(*(*string)(s), 10, 64)
+func (s Convert) ToInt() (int, error) {
+	ret, err := strconv.ParseInt((string)(s), 10, 64)
 	if err != nil {
 		return 0, err
 	}
 	return int(ret), nil
 }
 
-func (s *Convert) ToInt64() (int64, error) {
-	ret, err := strconv.ParseInt(*(*string)(s), 10, 64)
+func (s Convert) ToInt64() (int64, error) {
+	ret, err := strconv.ParseInt((string)(s), 10, 64)
 	if err != nil {
 		return 0, err
 	}
 	return ret, nil
 }
 
-func (s *Convert) ToUInt() (uint, error) {
-	ret, err := strconv.ParseUint(*(*string)(s), 10, 64)
+func (s Convert) ToUInt() (uint, error) {
+	ret, err := strconv.ParseUint((string)(s), 10, 64)
 	if err != nil {
 		return 0, err
 	}
 	return uint(ret), nil
 }
 
-func (s *Convert) ToUInt64() (uint64, error) {
-	ret, err := strconv.ParseUint(*(*string)(s), 10, 64)
+func (s Convert) ToUInt64() (uint64, error) {
+	ret, err := strconv.ParseUint((string)(s), 10, 64)
 	if err != nil {
 		return 0, err
 	}
 	return ret, nil
 }
 
-func (s *Convert) ToFloat() (float64, error) {
-	ret, err := strconv.ParseFloat(*(*string)(s), 64)
+func (s Convert) ToFloat() (float64, error) {
+	ret, err := strconv.ParseFloat((string)(s), 64)
 	if err != nil {
 		return 0, err
 	}
 	return ret, nil
 }
 
-func (s *Convert) ToBool() (bool, error) {
-	ret, err := strconv.ParseBool(*(*string)(s))
+func (s Convert) ToBool() (bool, error) {
+	ret, err := strconv.ParseBool((string)(s))
 	if err != nil {
 		return false, err
 	}
