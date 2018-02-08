0ac994a2f98f748874d1b015cb4c3cfc9215b708
snower <snower@snower.org>
2018-02-08 18:27:17 +0800

    将regex和其他的那种模式分离开

--- mux_node.go
+++ mux_node.go
@@ -12,6 +12,7 @@ type (
 		handler    Handler
 		text_subs  []mux_node
 		regex_subs []mux_node
+		other_subs []mux_node
 		any_sub    *any_node
 	}
 
@@ -22,6 +23,7 @@ type (
 
 	any_node struct {
 		base_node
+		name string
 	}
 
 	regex_node struct {
@@ -30,8 +32,9 @@ type (
 		re   *regexp.Regexp
 	}
 
-	wtf_mux struct {
-		nodes map[string]mux_node
+	other_node struct {
+		base_node
+		name string
 	}
 )
 
@@ -61,11 +64,15 @@ func parse_path(path string, h Handler) mux_node {
 		if pos != -1 {
 			left := path[pos:]
 			name := path[:pos]
-			ret := new_regex_node(name, nil, fmt.Sprintf("(?P<%s>[\\w\\-\\~]+)", name[1:]))
-			ret.set_sub_node(parse_path(left, h))
+			ret := new_other_node(name, nil)
+			if len(left) > 0 {
+				ret.set_sub_node(parse_path(left, h))
+			} else {
+				ret.handler = h
+			}
 			return ret
 		}
-		return new_regex_node(path, h, fmt.Sprintf("(?P<%s>[\\w\\-\\~]+)", path[1:]))
+		return new_other_node(path, h)
 	case '(':
 		left := 0
 		for i, c := range path {
@@ -101,14 +108,71 @@ func parse_path(path string, h Handler) mux_node {
 	return nil
 }
 
+func (bn *base_node) match_sub_nodes(path string, up RESTParams) (bool, Handler, RESTParams) {
+	if path == "" {
+		return true, bn.handler, up
+	}
+	for _, mux := range bn.text_subs {
+		if m, h, rup := mux.match(path, up); m {
+			if h != nil {
+				return true, h, rup
+			} else {
+				break
+			}
+		}
+	}
+	for _, mux := range bn.regex_subs {
+		if _, h, rup := mux.match(path, up); h != nil {
+			return true, h, rup
+		}
+	}
+	for _, mux := range bn.other_subs {
+		if _, h, rup := mux.match(path, up); h != nil {
+			return true, h, rup
+		}
+	}
+	if bn.any_sub != nil {
+		return true, bn.any_sub.handler, up
+	}
+	return true, nil, up
+
+}
+
+func (bn *base_node) merge_sub_node(path string, h Handler) {
+	if len(path) > 0 {
+		for _, s := range bn.text_subs {
+			if s.merge(path, h) {
+				return
+			}
+		}
+		for _, s := range bn.regex_subs {
+			if s.merge(path, h) {
+				return
+			}
+		}
+		for _, s := range bn.other_subs {
+			if s.merge(path, h) {
+				return
+			}
+		}
+		bn.set_sub_node(parse_path(path, h))
+	} else {
+		bn.handler = h
+	}
+}
+
 func (bn *base_node) set_sub_node(m mux_node) {
+	if m == nil {
+		return
+	}
 	switch m.(type) {
 	case *text_node:
 		bn.text_subs = append(bn.text_subs, m)
 	case *regex_node:
 		bn.regex_subs = append(bn.regex_subs, m)
+	case *other_node:
+		bn.other_subs = append(bn.other_subs, m)
 	case *any_node:
-		bn.handler = m.(*any_node).handler
 		bn.any_sub = m.(*any_node)
 	}
 }
@@ -117,11 +181,51 @@ func (bn *base_node) deep_clone_from(src *base_node) {
 	bn.pattern, bn.handler, bn.any_sub = src.pattern, src.handler, src.any_sub
 	bn.text_subs = make([]mux_node, 0, max(len(src.text_subs), 10))
 	bn.regex_subs = make([]mux_node, 0, max(len(src.regex_subs), 10))
+	bn.other_subs = make([]mux_node, 0, max(len(src.other_subs), 10))
 	for _, s := range src.text_subs {
 		bn.text_subs = append(bn.text_subs, s.deep_clone())
 	}
 	for _, s := range src.regex_subs {
 		bn.regex_subs = append(bn.regex_subs, s.deep_clone())
+	}
+	for _, s := range src.other_subs {
+		bn.other_subs = append(bn.other_subs, s.deep_clone())
+	}
+	if src.any_sub != nil {
+		node := src.any_sub.deep_clone()
+		bn.any_sub = node.(*any_node)
+	}
+}
+
+func (bn *base_node) dump(pre string) {
+	fmt.Print(pre, "-", bn.pattern)
+	if bn.handler != nil {
+		fmt.Print(" true ")
+	}
+	fmt.Println()
+	for _, s := range bn.text_subs {
+		if s == nil {
+			fmt.Println(pre + "  text nil, shit!!!")
+		} else {
+			s.dump(pre + " t ")
+		}
+	}
+	for _, s := range bn.regex_subs {
+		if s == nil {
+			fmt.Println(pre + "  regex is nil, shit!!!")
+		} else {
+			s.dump(pre + " r ")
+		}
+	}
+	for _, s := range bn.other_subs {
+		if s == nil {
+			fmt.Println(pre + "  other is nil, shit!!!")
+		} else {
+			s.dump(pre + " o ")
+		}
+	}
+	if bn.any_sub != nil {
+		bn.any_sub.dump(pre + " a ")
 	}
 }
 
@@ -131,6 +235,7 @@ func new_any_node(h Handler) *any_node {
 	ret.handler = h
 	ret.text_subs = make([]mux_node, 0, 0)
 	ret.regex_subs = make([]mux_node, 0, 0)
+	ret.other_subs = make([]mux_node, 0, 0)
 	return ret
 }
 
@@ -141,6 +246,7 @@ func new_text_node(p string, h Handler) *text_node {
 	ret.handler = h
 	ret.text_subs = make([]mux_node, 0, 0)
 	ret.regex_subs = make([]mux_node, 0, 0)
+	ret.other_subs = make([]mux_node, 0, 0)
 	return ret
 }
 
@@ -163,6 +269,18 @@ func new_regex_node(p string, h Handler, reg ...string) *regex_node {
 	}
 	ret.text_subs = make([]mux_node, 0, 0)
 	ret.regex_subs = make([]mux_node, 0, 0)
+	ret.other_subs = make([]mux_node, 0, 0)
+	return ret
+}
+
+func new_other_node(p string, h Handler) *other_node {
+	ret := &other_node{}
+	ret.pattern = p
+	ret.handler = h
+	ret.name = p[1:]
+	ret.text_subs = make([]mux_node, 0, 0)
+	ret.regex_subs = make([]mux_node, 0, 0)
+	ret.other_subs = make([]mux_node, 0, 0)
 	return ret
 }
 
@@ -170,8 +288,8 @@ func (an *any_node) match(path string, _ RESTParams) (bool, Handler, RESTParams)
 	return true, an.handler, RESTParams{}
 }
 
-func (an *any_node) match_self(path string, _ RESTParams) (bool, Handler, string, RESTParams) {
-	return true, an.handler, "", RESTParams{}
+func (an *any_node) match_self(path string, _ RESTParams) (bool, string, RESTParams) {
+	return true, "", RESTParams{}
 }
 
 func (an *any_node) merge(path string, h Handler) bool {
@@ -185,42 +303,21 @@ func (an *any_node) deep_clone() mux_node {
 }
 
 func (tn *text_node) match(path string, up RESTParams) (bool, Handler, RESTParams) {
-	if m, h, p, _ := tn.match_self(path, up); m {
-		if len(p) > 0 {
-			for _, mux := range tn.text_subs {
-				if m, h, up := mux.match(p, up); m {
-					if h != nil {
-						return true, h, up
-					} else {
-						break
-					}
-				}
-			}
-			for _, mux := range tn.regex_subs {
-				if _, h, up := mux.match(p, up); h != nil {
-					return true, h, up
-				}
-			}
-			if tn.any_sub != nil {
-				return true, tn.any_sub.handler, up
-			}
-			return true, nil, up
-		} else {
-			return true, h, up
-		}
+	if m, p, _ := tn.match_self(path, up); m {
+		return tn.match_sub_nodes(p, up)
 	} else {
 		return false, nil, up
 	}
 }
 
-func (tn *text_node) match_self(path string, up RESTParams) (bool, Handler, string, RESTParams) {
+func (tn *text_node) match_self(path string, up RESTParams) (bool, string, RESTParams) {
 	if (tn.pattern[0] != path[0]) || (tn.p_len > len(path)) {
-		return false, nil, path, up
+		return false, path, up
 	}
 	if strings.HasPrefix(path, tn.pattern) {
-		return true, tn.handler, path[tn.p_len:], up
+		return true, path[tn.p_len:], up
 	}
-	return false, nil, path, up
+	return false, path, up
 }
 
 func (tn *text_node) split(i int) {
@@ -231,6 +328,27 @@ func (tn *text_node) split(i int) {
 }
 
 func (tn *text_node) merge(path string, h Handler) bool {
+	p_len := len(path)
+	s_len := tn.p_len
+	min_len := min(p_len, s_len)
+	pos := 0
+	for ; pos < min_len; pos++ {
+		if tn.pattern[pos] != path[pos] {
+			break
+		}
+	}
+	if pos == 0 {
+		return false
+	}
+	if pos != tn.p_len {
+		tn.split(pos)
+	}
+	left := path[pos:]
+	tn.merge_sub_node(left, h)
+	return true
+}
+
+func (tn *text_node) merge2(path string, h Handler) bool {
 	p_len := len(path)
 	s_len := tn.p_len
 	min_len := min(p_len, s_len)
@@ -269,49 +387,29 @@ func (tn *text_node) deep_clone() mux_node {
 }
 
 func (rn *regex_node) match(path string, up RESTParams) (bool, Handler, RESTParams) {
-	if m, h, p, rup := rn.match_self(path, up); m {
-		if len(p) > 0 {
-			for _, mux := range rn.text_subs {
-				if m, h, rup := mux.match(p, rup); m {
-					if h != nil {
-						return true, h, rup
-					} else {
-						break
-					}
-				}
-			}
-			for _, mux := range rn.regex_subs {
-				if _, h, rup := mux.match(p, rup); h != nil {
-					return true, h, rup
-				}
-			}
-			if rn.any_sub != nil {
-				return true, rn.any_sub.handler, rup
-			}
-			return true, nil, rup
-		}
-		return true, h, rup
+	if m, p, rup := rn.match_self(path, up); m {
+		return rn.match_sub_nodes(p, rup)
 	} else {
 		return false, nil, rup
 	}
 }
 
-func (rn *regex_node) match_self(path string, up RESTParams) (bool, Handler, string, RESTParams) {
+func (rn *regex_node) match_self(path string, up RESTParams) (bool, string, RESTParams) {
 	pos := rn.re.FindStringIndex(path)
 	if pos != nil {
-		return true, rn.handler, path[pos[1]:], up.Append(rn.name, path[:pos[1]])
+		return true, path[pos[1]:], up.Append(rn.name, path[:pos[1]])
 	}
-	return false, nil, path, up
+	return false, path, up
 }
 
 func (rn *regex_node) merge(path string, h Handler) bool {
-	p_len := len(path)
-	s_len := len(rn.pattern)
-	if p_len >= s_len && rn.pattern == path[:s_len] {
+	if strings.HasPrefix(path, rn.pattern) {
+		p_len := len(path)
+		s_len := len(rn.pattern)
 		if p_len == s_len {
 			rn.handler = h
 		} else {
-			rn.set_sub_node(parse_path(path[s_len:], h))
+			rn.merge_sub_node(path[s_len:], h)
 		}
 		return true
 	}
@@ -323,5 +421,44 @@ func (rn *regex_node) deep_clone() mux_node {
 	ret.deep_clone_from(&rn.base_node)
 	ret.re = rn.re
 	ret.name = rn.name
+	return ret
+}
+
+func (on *other_node) match_self(path string, up RESTParams) (bool, string, RESTParams) {
+	fmt.Println("处理路径： ", path)
+	pos := strings.Index(path, "/")
+	if pos == -1 {
+		return true, "", up.Append(on.name, path)
+	}
+	fmt.Println("key = ", on.name, " , value = ", path[:pos])
+	return true, path[pos:], up.Append(on.name, path[:pos])
+}
+
+func (on *other_node) match(path string, up RESTParams) (bool, Handler, RESTParams) {
+	if m, p, rup := on.match_self(path, up); m {
+		return on.match_sub_nodes(p, rup)
+	} else {
+		return false, nil, rup
+	}
+}
+
+func (on *other_node) merge(path string, h Handler) bool {
+	if strings.HasPrefix(path, on.pattern) {
+		p_len := len(path)
+		s_len := len(on.pattern)
+		if p_len == s_len {
+			on.handler = h
+		} else {
+			on.merge_sub_node(path[s_len:], h)
+		}
+		return true
+	}
+	return false
+}
+
+func (on *other_node) deep_clone() mux_node {
+	ret := &other_node{}
+	ret.deep_clone_from(&on.base_node)
+	ret.name = on.name
 	return ret
 }
--- response.go
+++ response.go
@@ -7,39 +7,39 @@ import (
 )
 
 type (
-	WTFResponse struct {
+	wtf_response struct {
 		Context
 	}
 )
 
-func new_response(Context Context) Response {
-	ret := &WTFResponse{}
+func NewResponse(Context Context) Response {
+	ret := &wtf_response{}
 	ret.Context = Context
 	return ret
 }
 
-func (resp *WTFResponse) StatusCode(code int, body ...string) {
+func (resp *wtf_response) StatusCode(code int, body ...string) {
 	resp.Context.WriteHeader(code)
 	if len(body) > 0 {
 		resp.Context.WriteString(body[0])
 	}
 }
 
-func (resp *WTFResponse) NotFound(body ...string) {
+func (resp *wtf_response) NotFound(body ...string) {
 	resp.StatusCode(http.StatusNotFound, body...)
 }
 
-func (resp *WTFResponse) Redirect(url string) {
+func (resp *wtf_response) Redirect(url string) {
 	resp.Context.Header().Set("Location", url)
 	resp.StatusCode(http.StatusMovedPermanently)
 }
 
-func (resp *WTFResponse) Follow(url string, body ...string) {
+func (resp *wtf_response) Follow(url string, body ...string) {
 	resp.Context.Header().Set("Location", url)
 	resp.StatusCode(http.StatusSeeOther, body...)
 }
 
-func (resp *WTFResponse) CrossOrigin(domain ...string) {
+func (resp *wtf_response) CrossOrigin(domain ...string) {
 	if len(domain) > 0 {
 		resp.Context.Header().Set("Access-Control-Allow-Origin", domain[0])
 		resp.Context.Header().Set("Access-Control-Allow-Credentialls", "true")
@@ -54,7 +54,7 @@ func (resp *WTFResponse) CrossOrigin(domain ...string) {
 	}
 }
 
-func (resp *WTFResponse) WriteJson(obj interface{}) (int, error) {
+func (resp *wtf_response) WriteJson(obj interface{}) (int, error) {
 	data, e := json.Marshal(obj)
 	if e != nil {
 		return 0, e
@@ -63,7 +63,7 @@ func (resp *WTFResponse) WriteJson(obj interface{}) (int, error) {
 	return resp.Context.Write(data)
 }
 
-func (resp *WTFResponse) WriteXml(obj interface{}) (int, error) {
+func (resp *wtf_response) WriteXml(obj interface{}) (int, error) {
 	data, e := xml.Marshal(obj)
 	if e != nil {
 		return 0, e
